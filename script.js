30b1c132aa4629b65d2df77441614a65a87f1326
Basile <basile08@hotmail.fr>
2021-09-21 20:30:44 +0200

    Mise en place des compteurs incrémentaux
    +MàJ OnHover Scaling

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 
     window.onload = function(){
+        
+        //#region  Var Compteurs
+        var c2 = 0;
+        var c3 = 0;
+        var c4 = 0;
+        var c5 = 0;
+        var c6 = 0;
+        var c7 = 0;
+        var c8 = 0;
+        var c9 = 0;
+        var c10 = 0;
+        var c11 = 0;
+        var c12 = 0;
+        var c13 = 0;
+        var c14 = 0;
+        //#endregion
+
+        //#region MaxCards
         var nbrSabots = prompt("Combien de sabots? (0.5, 1, 2, 3,..)");
         document.getElementById("nbrSabotVar").innerHTML = nbrSabots;
-        /*document.getElementById("max").innerHTML = nbrSabots*4;*/
         var maxCards = nbrSabots*4;
         document.getElementById('max1').innerHTML = maxCards;
         document.getElementById('max2').innerHTML = maxCards;
@@ -17,25 +34,87 @@
         document.getElementById('max11').innerHTML = maxCards;
         document.getElementById('max12').innerHTML = maxCards;
         document.getElementById('max13').innerHTML = maxCards;
+        //#endregion
 
-        var c2 = 0;
-        var c3 = 0;
-        var c4 = 0;
-        var c5 = 0;
-        var c6 = 0;
-        var c7 = 0;
-        var c8 = 0;
-        var c9 = 0;
-        var c10 = 0;
-        var c11 = 0;
-        var c12 = 0;
-        var c13 = 0;
-        var c14 = 0;
+        //#region  OnClick --> Incr
+        document.getElementById('nbrProc2').addEventListener("click", incr2);
+        function incr2() {
+          c2 += 1;
+          document.getElementById('c2').innerHTML = c2;
+        }
+        document.getElementById('nbrProc3').addEventListener("click", incr3);
+        function incr3() {
+          c3 += 1;
+          document.getElementById('c3').innerHTML = c3;
+        }
+        document.getElementById('nbrProc4').addEventListener("click", incr4);
+        function incr4() {
+          c4 += 1;
+          document.getElementById('c4').innerHTML = c4;
+        }
+        document.getElementById('nbrProc5').addEventListener("click", incr5);
+        function incr5() {
+          c5 += 1;
+          document.getElementById('c5').innerHTML = c5;
+        }
+        document.getElementById('nbrProc6').addEventListener("click", incr6);
+        function incr6() {
+          c6 += 1;
+          document.getElementById('c6').innerHTML = c6;
+        }
+        document.getElementById('nbrProc7').addEventListener("click", incr7);
+        function incr7() {
+          c7 += 1;
+          document.getElementById('c7').innerHTML = c7;
+        }
+        document.getElementById('nbrProc8').addEventListener("click", incr8);
+        function incr8() {
+          c8 += 1;
+          document.getElementById('c8').innerHTML = c8;
+        }
+        document.getElementById('nbrProc9').addEventListener("click", incr9);
+        function incr9() {
+          c9 += 1;
+          document.getElementById('c9').innerHTML = c9;
+        }
+        document.getElementById('nbrProc10').addEventListener("click", incr10);
+        function incr10() {
+          c10 += 1;
+          document.getElementById('c10').innerHTML = c10;
+        }
+        document.getElementById('nbrProc11').addEventListener("click", incr11);
+        function incr11() {
+          c11 += 1;
+          document.getElementById('c11').innerHTML = c11;
+        }
+        document.getElementById('nbrProc12').addEventListener("click", incr12);
+        function incr12() {
+          c12 += 1;
+          document.getElementById('c12').innerHTML = c12;
+        }
+        document.getElementById('nbrProc13').addEventListener("click", incr13);
+        function incr13() {
+          c13 += 1;
+          document.getElementById('c13').innerHTML = c13;
+        }
+        document.getElementById('nbrProc14').addEventListener("click", incr14);
+        function incr14() {
+          c14 += 1;
+          document.getElementById('c14').innerHTML = c14;
+        }
+        //#endregion
+
+        
     }
 
 
-    figure.onclick = function(){
-    }
+
+
+    
+      
+    
+    
+
 
 
 
