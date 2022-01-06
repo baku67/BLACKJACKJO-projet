3e2e0ea0777fbce1994703419df96eebd06b9f96
Basile <basile08@hotmail.fr>
2022-01-06 23:34:22 +0100

    Désactivation ScoreHiLo + statut connexion db on footer

--- index.php
+++ index.php
@@ -15,39 +15,7 @@
 	</head>
 
 	
-	<?php
-	include("config.php");
-
-	session_start();
 	
-	if($_SERVER["REQUEST_METHOD"] == "POST") {
-		// username and password sent from form 
-		
-		$myusername = mysqli_real_escape_string($db,$_POST['username']);
-		$mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-		
-		$sql = "SELECT id FROM users WHERE username = '$myusername' and password = '$mypassword'";
-		$result = mysqli_query($db,$sql);
-		$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-		$active = $row['active'];
-		
-		$count = mysqli_num_rows($result);
-		
-		// If result matched $myusername and $mypassword, table row must be 1 row
-			
-		if($count == 1) {
-			session_register("myusername");
-			$_SESSION['login_user'] = $myusername;
-			
-			header("location: welcome.php");
-			echo("CA MARCHE");
-		}
-		else {
-			$error = "Your Login Name or Password is invalid";
-			echo($error);
-		}
-	}
-	?>
 
 	<body>
 
@@ -164,10 +132,43 @@
 			</div>
 
 
-
+			
 
 			<div id="footer">
 
+				<?php
+				include("config.php");
+
+				session_start();
+				
+				if($_SERVER["REQUEST_METHOD"] == "POST") {
+					// username and password sent from form 
+					
+					$myusername = mysqli_real_escape_string($db,$_POST['username']);
+					$mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+					
+					$sql = "SELECT id FROM users WHERE username = '$myusername' and password = '$mypassword'";
+					$result = mysqli_query($db,$sql);
+					$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+					$active = $row['active'];
+					
+					$count = mysqli_num_rows($result);
+					
+					// If result matched $myusername and $mypassword, table row must be 1 row
+						
+					if($count == 1) {
+						session_register("myusername");
+						$_SESSION['login_user'] = $myusername;
+						
+						header("location: welcome.php");
+						echo("CA MARCHE");
+					}
+					else {
+						$error = "Your Login Name or Password is invalid <br>";
+						echo($error);
+					}
+				}
+				?>
 
 				<div style="display:inline-flex;">
 					<h3 id="footerTitle" style="letter-spacing: 1px;">  Comment jouer ...  </h3>
